eca7365149505ef732fe228155050782dd5d811d
Beckmann, Anton <anton.beckmann@cgi.com>
2020-06-12 23:23:18 +0200

    some work done

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM ubuntu:18.04
 RUN apt-get update && apt-get install -y ssmtp
 ADD ./ssmtp.conf /etc/ssmtp/ssmtp.conf
 ADD entrypoint.sh entrypoint.sh
-
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
 username="$1"
-password="S2"
+password="$2"
+mailaddresses="$3"
 echo
-curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" |  grep -oPm1 "(?<=<title>)[^<]+" | sed '1d'
+
+
+iparray=($(curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" |  grep -oPm1 "(?<=<title>)[^<]+" | sed '1d')) 
+currentIPV4="${iparray[1]}"
+
+
+set -e
+{
+	    echo "Hallo Hasenfreunde,"
+		echo ""
+        echo "die neue Adresse für die Rabbitcam lautet:"
+        echo "http://$currentIPV4:8081"
+        echo ""
+        echo ""
+        echo "Viel Freude"
+        echo "Anton und Sandra"
+
+        
+} > /tmp/email.txt
+
+IFS=',' read -r -a mailarray <<< "$mailaddresses"
+
+for element in "${mailarray[@]}"
+do
+    sendmail antonbeckmann85@gmail.com  < /tmp/email.txt
+done
